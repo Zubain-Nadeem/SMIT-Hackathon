149439da94614cd7a420302a1e83ec95bc8f32dc
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-10-18 19:51:12 +0500

    Smit-hackathone

---
+++ src/Config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADOk-MuA57LkMK_lOQfQq9VaCc1kcpUxw",
+  authDomain: "smit-hackthon-7ed13.firebaseapp.com",
+  projectId: "smit-hackthon-7ed13",
+  storageBucket: "smit-hackthon-7ed13.firebasestorage.app",
+  messagingSenderId: "869982239697",
+  appId: "1:869982239697:web:2b316b6d0e59b7168abef0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
